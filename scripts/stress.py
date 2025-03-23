122d039d8b02bd0fbbea17cdd52d135a87f53e74
Rory Linehan <rorylinehan@protonmail.com>
2025-03-23 12:04:27 -0500

    Round records/s metric to an integer in stress.py output

--- scripts/stress.py
+++ scripts/stress.py
@@ -75,6 +75,6 @@ if __name__ == '__main__':
             print(f"failed to update records atomically")
         else:
             print(f"all records updated atomically")
-        print(f"updated record {user_id} {len(results)} times in {duration} seconds at {len(results) / duration} records/s")
+        print(f"updated record {user_id} {len(results)} times in {duration} seconds at ~{int(len(results) / duration)} records/s")
     else:
         print("failed to create user")
