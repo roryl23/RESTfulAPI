bec7be6c65e9bd544294de7fc3a4a57f464d4126
Rory Linehan <rorylinehan@protonmail.com>
2025-03-23 11:25:38 -0500

    Add concurrent updates with ThreadPoolExecutor in stress test

--- scripts/stress.py
+++ scripts/stress.py
@@ -1,5 +1,9 @@
 import argparse
+import time
+
 import requests
+from concurrent import futures
+from concurrent.futures import ThreadPoolExecutor
 
 
 def parse_args():
@@ -27,7 +31,7 @@ class Stress:
 
     def create_user(self):
         r = requests.post(f"{self.url}/users", json={
-            'name': 'test',
+            'name': 'testuser',
             'email': 'testuser@test.net',
         })
         if r.status_code == 201:
@@ -37,14 +41,27 @@ class Stress:
             print(f"failed to create user: {r.status_code}")
             return None
 
-    def update_records(self, user_id: str, num: int=100):
-        for i in range(num):
-            r = requests.put(f"{self.url}/users/{user_id}", json={
-                'name': f"test{i}",
-                'email': f"testuser{i}@test.net",
-            })
-            if r.status_code != 200:
-                print(f"failed to update record: {r.status_code}")
+    def update_record(self, user_id: str, i: int):
+        r = requests.put(f"{self.url}/users/{user_id}", json={
+            'name': f"testuser{i}",
+            'email': f"testuser{i}@test.net",
+        })
+        if r.status_code == 200:
+            return True
+        else:
+            return False
+
+    def update_records(self, user_id: str, n: int=10000, w: int=64):
+        with ThreadPoolExecutor(max_workers=w) as executor:
+            threads = [executor.submit(self.update_record, user_id, i) for i in range(1, n)]
+            results = []
+            for future in futures.as_completed(threads):
+                try:
+                    result = future.result()
+                    results.append(result)
+                except Exception as e:
+                    print(f"thread generated an exception: {e}")
+            return results
 
 
 if __name__ == '__main__':
@@ -53,6 +70,14 @@ if __name__ == '__main__':
     stresser = Stress(args.host, args.port)
     user_id = stresser.create_user()
     if user_id is not None:
-        stresser.update_records(user_id)
+        print(f"created user, running concurrent update stress test...")
+        start_time = time.time()
+        results = stresser.update_records(user_id)
+        end_time = time.time()
+        duration = end_time - start_time
+        if not all(results):
+            print(f"failed to update records atomically in {duration} seconds")
+        else:
+            print(f"all records updated atomically in {duration} seconds")
     else:
         print("failed to create user")
